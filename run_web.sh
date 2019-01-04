edceee7e9f0c5600e0186a05b1dbb574e1c22076
turkey <zaihuazhao@163.com>
2019-01-04 20:07:01 +0800

    add gunicorn.ini

--- run_web.sh
+++ run_web.sh
@@ -2,10 +2,11 @@
 
 python manage.py migrate
 python manage.py collectstatic --noinput
-#PIDPATH="/tmp/uwsgi.pid"
-#if [ -f ${PIDPATH} ]
-#then
-#    cat ${PIDPATH} | xargs kill -9
-#fi
-#uwsgi --ini uwsgi_deploy.ini
-python manage.py runserver 0.0.0.0:8000
+PIDPATH="/tmp/uwsgi.pid"
+if [ -f ${PIDPATH} ]
+then
+    uwsgi --stop ${PIDPATH}
+fi
+gunicorn -c gunicorn.ini LV.wsgi
+#uwsgi --ini ./uwsgi_deploy.ini
+#python manage.py runserver 0.0.0.0:8000
