788642d7d575021e2edae52958acd0491d06cb80
turkey <zaihuazhao@163.com>
2019-01-04 16:47:40 +0800

    fix uwsgi

--- run_web.sh
+++ run_web.sh
@@ -2,5 +2,10 @@
 
 python manage.py migrate
 python manage.py collectstatic --noinput
-uwsgi uwsgi_deploy.ini
-#python manage.py runserver 0.0.0.0:8000
+#PIDPATH="/tmp/uwsgi.pid"
+#if [ -f ${PIDPATH} ]
+#then
+#    cat ${PIDPATH} | xargs kill -9
+#fi
+#uwsgi --ini uwsgi_deploy.ini
+python manage.py runserver 0.0.0.0:8000
