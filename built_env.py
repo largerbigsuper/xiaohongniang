7bd4bc1153b2702d2e5f3389b5b655ba4357798a
turkey <zaihuazhao@163.com>
2019-01-29 19:22:37 +0800

    modify pip

--- built_env.py
+++ built_env.py
@@ -5,13 +5,19 @@
 # @Email   : zaihuazhao@163.com
 # @File    : built_env.py
 from subprocess import call
+call(['pip', 'install', '--upgrade', 'pip',
+      '-i', 'http://mirrors.aliyun.com/pypi/simple/', '--trusted-host', 'mirrors.aliyun.com'])
 
 # step1
-call(['pip', 'install', '-r', 'requirements.txt'])
+call(['pip', 'install', '-r', 'requirements.txt',
+      '-i', 'http://mirrors.aliyun.com/pypi/simple/', '--trusted-host', 'mirrors.aliyun.com'])
 # step2
 # call(['pip', 'install', 'git+https://github.com/htwenning/aliyun-python-sdk-core'])
-call(['pip', 'install', 'git+https://github.com/htwenning/aliyun-python-sdk-dysmsapi'])
-call(['pip', 'install', 'git+https://github.com/rongcloud/server-sdk-python.git'])
-
+# call(['pip', 'install', 'git+https://github.com/htwenning/aliyun-python-sdk-dysmsapi'])
+# call(['pip', 'install', 'git+https://github.com/rongcloud/server-sdk-python.git'])
+aliyun_python_sdk_core = 'python-lib/aliyun-python-sdk-core'
+aliyun_python_sdk_dysmsapi = 'python-lib/aliyun-python-sdk-dysmsapi'
+call(['pip', 'install', 'aliyun_python_sdk_dysmsapi'])
+call(['pip', 'install', 'aliyun_python_sdk_core'])
 
 
---
+++ python-lib/aliyun-python-sdk-dysmsapi/README.md
@@ -0,0 +1,24 @@
+aliyun-python-sdk-dysmsapi
+===
+This is the dysmsapi module of Aliyun Python SDK.
+
+从阿里云短信服务sdk下载, 用于方便写到requirements中. 
+使用短信服务需要安装下面两个库
+
+安装方法:
+===
+
+```bash
+pip install git+https://github.com/htwenning/aliyun-python-sdk-core
+pip install git+https://github.com/htwenning/aliyun-python-sdk-dysmsapi
+```
+
+
+Aliyun Python SDK is the official software development kit. It makes things easy to integrate your Python application, library, or script with Aliyun services.
+
+This module works on Python versions:
+
+2.6.5 and greater
+Documentation:
+
+Please visit http://develop.aliyun.com/sdk/python
