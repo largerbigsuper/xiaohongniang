3b62269518f8efd112cdc44d8596d5750a9e91ae
turkey <zaihuazhao@163.com>
2019-03-04 14:21:28 +0800

    add virtual service type

--- datamodels/notices/admin.py
+++ datamodels/notices/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from datamodels.notices.models import Notice
+from datamodels.notices.models import Notice, Demand, mm_Demand
+from datamodels.products.models import mm_VirtualService
 
 
 @admin.register(Notice)
@@ -10,3 +11,21 @@ class NoticeAdmin(admin.ModelAdmin):
     list_display = ('id', 'action_type', 'content_type', 'object_id',
                     'content_type_result', 'result_id',
                     'from_customer', 'to_customer', 'status')
+
+
+@admin.register(Demand)
+class Demand(admin.ModelAdmin):
+
+    list_per_page = 20
+    list_display = ('id', 'demand_type', 'customer', 'to_customer', 'status', 'create_at')
+    list_filter = ('demand_type', 'status', 'create_at')
+
+    def save_model(self, request, obj, form, change):
+        # 返回用户的服务卡
+        if 'status' in form.changed_data:
+            if form.initial['status'] == 0:
+                if form.cleaned_data['status'] == mm_Demand.Status_Refused:
+                    service_type = mm_VirtualService.Demand_Type_2_Service_Type.get(obj.demand_type, 0)
+                    mm_VirtualService.modify_card(obj.customer_id, service_type, 1)
+        super().save_model(request, obj, form, change)
+
--- datamodels/products/views.py
+++ datamodels/products/views.py
@@ -6,7 +6,8 @@ from django.db import transaction
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
-from datamodels.products.models import mm_AlipayOrder, mm_ServiceCertification, mm_CustomerOrder
+from datamodels.products.models import mm_AlipayOrder, mm_ServiceCertification, mm_CustomerOrder, mm_VirtualService
+from datamodels.role.models import mm_Customer
 from lib.pay import alipay_serve
 
 logger = logging.getLogger('products')
@@ -47,13 +48,18 @@ class AliPayNotifyView(APIView):
                     order.status = mm_AlipayOrder.ORDER_STATU_DONE
                     order.trade_no = data['trade_no']
                     order.save()
-                    price_info = json.loads(order.virtual_service.pricelist)[order.price_index]
-                    days = price_info['days']
-                    service_name = order.virtual_service.name
-                    price_index_name = price_info['name']
-                    mm_CustomerOrder.add_order(order.customer, 1, order, out_trade_no, service_name, price_index_name,
-                                               total_amount)
-                    mm_ServiceCertification.update_certification(order.customer_id, order.virtual_service, days)
+                    if order.virtual_service.service_type in mm_VirtualService.Service_Group_Vip:
+                        price_info = json.loads(order.virtual_service.pricelist)[order.price_index]
+                        days = price_info['days']
+                        service_name = order.virtual_service.name
+                        price_index_name = price_info['name']
+                        mm_CustomerOrder.add_order(order.customer, 1, order, out_trade_no, service_name,
+                                                   price_index_name, total_amount)
+                        mm_ServiceCertification.update_certification(order.customer_id, order.virtual_service, days)
+                    elif order.virtual_service.service_type in mm_VirtualService.Service_Group_Card:
+                        mm_VirtualService.modify_card(order.customer_id, order.virtual_service.service_type, 1)
+                    else:
+                        pass
             except:
                 logger.error('Error: %s ' % traceback.format_exc())
             finally:
